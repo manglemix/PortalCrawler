e1404793b93900314ac4dbb8e0e626abc96b1f97
Highjacker36 <91640675+Highjacker36@users.noreply.github.com>
2024-06-04 20:05:36 -0600

    merged tuesday work

---
+++ levels/test/3dportal.gd
@@ -0,0 +1,31 @@
+extends StaticBody3D
+
+
+var collider
+var cZOffset
+var cXOffset
+var p
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	collider = $CollisionShape2D
+	cZOffset = self.get_global_position().z
+	cXOffset = self.get_global_position().x
+	print(cZOffset)
+	print(cXOffset)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+	
+func _set_partner(partner):
+	p = partner
+
+
+func _on_area_3d_body_entered(body):
+	var bodyY = body.get_global_position().y
+	var bodyX = body.get_global_position().x
+	var bodyZ = body.get_global_position().z
+	body.set_global_position(Vector3((p.cXOffset - (bodyX - cXOffset - .05)), bodyY, (p.cZOffset - (bodyZ - cZOffset - .05))))
---
+++ levels/test/Player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 5
+
+@onready var first = true
+
+@onready var counter = 0
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	if (first):
+		get_parent().get_node("leftPortal")._set_partner(get_parent().get_node("rightPortal"))
+		get_parent().get_node("rightPortal")._set_partner(get_parent().get_node("leftPortal"))
+		first = false
+	
+	if (counter > 100):
+		print(self.get_global_position())
+		counter = 0
+	counter += 1
+	
+	# We create a local variable to store the input direction.
+	var direction = Vector3.ZERO
+
+	# We check for each move input and update the direction accordingly.
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("down"):
+		# Notice how we are working with the vector's x and z axes.
+		# In 3D, the XZ plane is the ground plane.
+		direction.z += 1
+	if Input.is_action_pressed("up"):
+		direction.z -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
