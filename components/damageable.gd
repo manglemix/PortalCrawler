3d3f3248beb78f5c5640ffb96df03e3767b69a1e
Najman <najman.husaini@utah.edu>
2024-06-04 18:34:36 -0600

    test level and health

---
+++ components/damageable.gd
@@ -0,0 +1,30 @@
+@tool
+class_name Damageable
+extends Node
+
+
+signal damaged(health_change: int)
+
+
+@export var damage_multiplier := 1.0
+
+func _get_configuration_warnings() -> PackedStringArray:
+	if name != "Damageable":
+		return ["Name must be Damageable"]
+	return []
+
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		renamed.connect(update_configuration_warnings)
+
+
+func damage_once(damage: int):
+	damaged.emit(roundi(damage * damage_multiplier))
+
+
+func damage_node_once(node: Node, damage: int):
+	var damageable: Damageable = node.get_node_or_null("Damageable")
+	if damageable == null:
+		return
+	damageable.damage_once(damage)
