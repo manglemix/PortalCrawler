bc396ddbc00104b939fbb89969a16400dd823a07
TrevorTresner <u1300846@utah.edu>
2024-06-06 16:39:39 -0600

    working portals

--- levels/test_zone/test/3dportal.gd
+++ levels/test_zone/test/3dportal.gd
@@ -6,25 +6,48 @@ var cZOffset
 var cXOffset
 var p
 
+@onready var cooldown = $cooldown
+
 var facing
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	collider = $CollisionShape2D
+	collider = $Area3D
 	cZOffset = self.get_global_position().z
 	cXOffset = self.get_global_position().x
 	
 	facing = ''
 
-
+func update_position():
+	cZOffset = self.get_global_position().z
+	cXOffset = self.get_global_position().x
+	
+func update_direction(direction):
+	facing = direction
+	
 func _set_partner(partner):
 	p = partner
-
-
-func _on_area_3d_body_entered(body):
-	var bodyY = body.get_global_position().y
-	var bodyX = body.get_global_position().x
-	var bodyZ = body.get_global_position().z
 	
-	
-	body.set_global_position(Vector3(p.cXOffset + .1, bodyY, p.cZOffset))
+func _physics_process(delta):
+	var bodies = collider.get_overlapping_bodies()
+	for body in bodies:
+		if (!check_velocity(body.target_velocity)):
+			return
+		if(p == null):
+			return
+		var bodyY = body.get_global_position().y
+		var bodyX = body.get_global_position().x
+		var bodyZ = body.get_global_position().z
+		body.set_global_position(Vector3(p.cXOffset, bodyY, p.cZOffset))
+		
+func check_velocity(velocity):
+	print(velocity)
+	if (facing == 'l'):
+		return velocity.x < 0
+	if (facing == 'r'):
+		return velocity.x > 0
+	if (facing == 'u'):
+		return velocity.z < 0
+	if (facing == 'd'):
+		return velocity.z > 0
+
