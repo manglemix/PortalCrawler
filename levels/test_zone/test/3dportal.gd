92373db04248178c7314fad3be82438b8eb474a8
TrevorTresner <u1300846@utah.edu>
2024-06-06 15:18:09 -0600

    added framework for shooting portals

--- levels/test_zone/test/3dportal.gd
+++ levels/test_zone/test/3dportal.gd
@@ -6,13 +6,15 @@ var cZOffset
 var cXOffset
 var p
 
+var facing
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	collider = $CollisionShape2D
 	cZOffset = self.get_global_position().z
 	cXOffset = self.get_global_position().x
-	print(cZOffset)
-	print(cXOffset)
+	
+	facing = ''
 
 
 func _set_partner(partner):
@@ -23,4 +25,6 @@ func _on_area_3d_body_entered(body):
 	var bodyY = body.get_global_position().y
 	var bodyX = body.get_global_position().x
 	var bodyZ = body.get_global_position().z
-	body.set_global_position(Vector3((p.cXOffset - (bodyX - cXOffset - .05)), bodyY, (p.cZOffset - (bodyZ - cZOffset - .05))))
+	
+	
+	body.set_global_position(Vector3(p.cXOffset + .1, bodyY, p.cZOffset))
--- levels/test_zone/test/Player.gd
+++ levels/test_zone/test/Player.gd
@@ -5,29 +5,44 @@ extends CharacterBody3D
 
 @onready var first = true
 
-@onready var counter = 0
+@onready var ray = $RayCast3D
+
+@onready var left = true
 
 var target_velocity = Vector3.ZERO
 
-func _physics_process(_delta):
-	if (first):
-		get_parent().get_node("leftPortal")._set_partner(get_parent().get_node("rightPortal"))
-		get_parent().get_node("rightPortal")._set_partner(get_parent().get_node("leftPortal"))
-		first = false
-	
-	if (counter > 100):
-		print(self.get_global_position())
-		counter = 0
-	counter += 1
-	
+func get_input():
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
-
+	if Input.is_action_just_pressed("shoot"):
+		var rayvector = Vector3.ZERO
+		if (ray.is_colliding()):
+			rayvector = ray.get_collision_normal()
+			if (rayvector.x > .7):
+				print("left")
+			elif (rayvector.x < -.7):
+				print("right")
+			elif (rayvector.z > .7):
+				print("up")
+			elif (rayvector.z < -.7):
+				print("down")
+			print(rayvector)
+		else:
+			print("no collision")
+	
+		
+			
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("right"):
 		direction.x += 1
+		if (!left):
+			ray.rotate_y(PI)
+			left = true
 	if Input.is_action_pressed("left"):
 		direction.x -= 1
+		if (left):
+			left = false
+			ray.rotate_y(PI)
 	if Input.is_action_pressed("down"):
 		# Notice how we are working with the vector's x and z axes.
 		# In 3D, the XZ plane is the ground plane.
@@ -44,4 +59,13 @@ func _physics_process(_delta):
 	
 	# Moving the Character
 	velocity = target_velocity
+
+func _physics_process(_delta):
+	if (first):
+		get_parent().get_node("leftPortal")._set_partner(get_parent().get_node("rightPortal"))
+		get_parent().get_node("rightPortal")._set_partner(get_parent().get_node("leftPortal"))
+		first = false
+	
+	
+	get_input()
 	move_and_slide()
