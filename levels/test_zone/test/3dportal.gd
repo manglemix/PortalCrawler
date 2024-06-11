4813a5b3accf9ed56a665e88ce590323270c493b
Highjacker36 <91640675+Highjacker36@users.noreply.github.com>
2024-06-10 20:40:18 -0600

    various code cleanup and commenting

--- levels/test_zone/test/3dportal.gd
+++ levels/test_zone/test/3dportal.gd
@@ -1,47 +1,52 @@
 extends StaticBody3D
 
 
-var collider
-var cZOffset
-var cXOffset
-var p
-
+# some nodes for easy access later
+@onready var collider = $Area3D
 @onready var cooldown = $cooldown
 
+# holds the portals partner for easy reference
+var p
 var facing
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	collider = $Area3D
-	cZOffset = self.get_global_position().z
-	cXOffset = self.get_global_position().x
-	
-	facing = ''
+# obviously could grab these values without storing them, but for
+# code readability and performance we store them after they are updated 1 time
+var cZOffset
+var cXOffset
 
+# these 2 functions are called 1 time by the player when the portal is being setup
+# they handle the portals position and rotation
 func update_position():
 	cZOffset = self.get_global_position().z
 	cXOffset = self.get_global_position().x
-	
 func update_direction(direction):
 	facing = direction
-	
+
+# gives this portal a reference to its partner portal, updated by the creation of
+# new portals
 func _set_partner(partner):
 	p = partner
-	
+
+
 func _physics_process(delta):
+	# if there is no partner then no work is needed, return
+	if(p == null):
+			return
+	# check all overlapping bodies to see if they need to be teleported
 	var bodies = collider.get_overlapping_bodies()
 	for body in bodies:
+		# SPECIFICALLY target_velocity must exist for objects to be teleported, normal velocity
+		# values do not work because the real velocity value will often drop to 0 when the object hits
+		# the wall the portal is on before being checked for teleportation
 		if (!check_velocity(body.target_velocity)):
 			return
-		if(p == null):
-			return
+		# get the objects y value because that does not change, then teleport it
 		var bodyY = body.get_global_position().y
-		var bodyX = body.get_global_position().x
-		var bodyZ = body.get_global_position().z
 		body.set_global_position(Vector3(p.cXOffset, bodyY, p.cZOffset))
 		
+# small helper function that checks if the object hitting
+# a portals collider is moving with any velocity into the portal
 func check_velocity(velocity):
-	print(velocity)
 	if (facing == 'l'):
 		return velocity.x < 0
 	if (facing == 'r'):
--- levels/test_zone/test/Player.gd
+++ levels/test_zone/test/Player.gd
@@ -1,80 +1,34 @@
 extends CharacterBody3D
 
-
-# How fast the player moves in meters per second.
+# exported vars
 @export var speed = 5
 
-@onready var first = true
-
-
-
+# stored nodes/scenes for easy reference
 @onready var ray = $RayCast3D
+var portal = preload("res://object_scenes/portal.tscn")
 
-var portal = preload("res://portal.tscn")
-
-var firstPortal
-var secondPortal
-
+# these are some simple flags for the portal placement code
 @onready var firstplaced = false
 @onready var secondplaced = false
 
-
+# general vars
+var firstPortal
+var secondPortal
 var target_velocity = Vector3.ZERO
 
+# hotfix for making sure the node is named properly, remove later
 func _ready():
 	name = "Player"
 
-func get_input():
-	# We create a local variable to store the input direction.
+# all input from the player is handled here
+func _input(event):
 	var direction = Vector3.ZERO
-	if Input.is_action_just_pressed("shoot"):
-		var rayvector = Vector3.ZERO
-		if (ray.is_colliding()):
-			var newportal = portal.instantiate()
-			if (!firstplaced):
-				firstPortal = newportal
-				firstplaced = true
-			elif (!secondplaced):
-				secondPortal = newportal
-				secondplaced = true
-				secondPortal._set_partner(firstPortal)
-				firstPortal._set_partner(secondPortal)
-			else:
-				firstPortal.queue_free()
-				firstPortal = secondPortal
-				secondPortal = newportal
-				secondPortal._set_partner(firstPortal)
-				firstPortal._set_partner(secondPortal)
-			
-			rayvector = ray.get_collision_normal()
-			var xposition = ray.get_collision_point()
-			
-			get_tree().root.get_child(0).add_child(newportal)
-			
-			newportal.set_global_position(xposition)
-			
-			if (rayvector.x > .7):
-				newportal.update_direction('l')
-			elif (rayvector.x < -.7):
-				newportal.set_rotation_degrees(Vector3(0, 180, 0))
-				newportal.update_direction('r')
-			elif (rayvector.z > .7):
-				newportal.set_rotation_degrees(Vector3(0, -90, 0))
-				newportal.set_global_position(Vector3(xposition.x, xposition.y, xposition.z))
-				newportal.update_direction('u')
-			elif (rayvector.z < -.7):
-				newportal.set_rotation_degrees(Vector3(0, 90, 0))
-				newportal.set_global_position(Vector3(xposition.x, xposition.y, xposition.z))
-				newportal.update_direction('d')
-				
-			newportal.update_position()
-			print(rayvector)
-		else:
-			print("no collision")
 	
-		
-			
-	# We check for each move input and update the direction accordingly.
+	# shooting portals
+	if Input.is_action_just_pressed("shoot"):
+		_create_portal()
+	
+	# movement
 	if Input.is_action_pressed("right"):
 		direction.x += 1
 		ray.set_rotation_degrees(Vector3(0, 0, 0))
@@ -92,20 +46,65 @@ func get_input():
 		direction.z -= 1
 		rotation.y = 0.0
 		
+	# applying movement
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		
-	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-	
-	# Moving the Character
 	velocity = target_velocity
 
 func _physics_process(_delta):
-	get_input()
 	move_and_slide()
 
 
 func _on_damaged(_health_change: int) -> void:
 	Shake.shake_node(get_viewport().get_camera_3d(), 0.25, 0.2, 5)
+	
+	
+# function that creates the portals when the player presses the button to do so
+# the reason why the player is handling this code is because all player inputs
+# should be handled by the player script when possible
+func _create_portal():
+	if (ray.is_colliding()):
+		# this block handles the constant cycling of portals as you spawn more and more
+		var newportal = portal.instantiate()
+		if (!firstplaced):
+			firstPortal = newportal
+			firstplaced = true
+		elif (!secondplaced):
+			secondPortal = newportal
+			secondplaced = true
+			secondPortal._set_partner(firstPortal)
+			firstPortal._set_partner(secondPortal)
+		else:
+			firstPortal.queue_free()
+			firstPortal = secondPortal
+			secondPortal = newportal
+			secondPortal._set_partner(firstPortal)
+			firstPortal._set_partner(secondPortal)
+		
+		# get the position to spawn the portal at
+		var rayvector = Vector3.ZERO
+		rayvector = ray.get_collision_normal()
+		var xposition = ray.get_collision_point()
+		
+		# place the new portal at the correct position
+		get_tree().root.get_child(0).add_child(newportal)
+		newportal.set_global_position(xposition)
+		
+		# rotate the portal correctly and make sure it knows what way it's facing (for later use)
+		if (rayvector.x > .7):
+			newportal.update_direction('l')
+		elif (rayvector.x < -.7):
+			newportal.set_rotation_degrees(Vector3(0, 180, 0))
+			newportal.update_direction('r')
+		elif (rayvector.z > .7):
+			newportal.set_rotation_degrees(Vector3(0, -90, 0))
+			newportal.set_global_position(Vector3(xposition.x, xposition.y, xposition.z))
+			newportal.update_direction('u')
+		elif (rayvector.z < -.7):
+			newportal.set_rotation_degrees(Vector3(0, 90, 0))
+			newportal.set_global_position(Vector3(xposition.x, xposition.y, xposition.z))
+			newportal.update_direction('d')
+		# make sure the portal updates its stored position
+		newportal.update_position()
