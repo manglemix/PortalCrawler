982adc3df2fea0fe6fc968a8136ef7e5319ff1ed
TrevorTresner <u1300846@utah.edu>
2024-06-20 16:56:44 -0600

    finished portal-player mechanics

--- components/teleportable.gd
+++ components/teleportable.gd
@@ -48,22 +48,28 @@ func _teleported(Portal1: Portal, Portal2: Portal):
 				get_parent().set_input_change(true, true, true)
 		elif (Portal1.facing == 'u'):
 			if (Portal2.facing == 'l'):
-				get_parent().rotation.y -= PI / 2
+				get_parent().upheld = true
+				get_parent().set_input_change(true, true, true)
 			elif (Portal2.facing == 'r'):
-				get_parent().rotation.y += PI / 2
+				get_parent().upheld = true
+				get_parent().set_input_change(true, false, false)
 			elif (Portal2.facing == 'u'):
-				get_parent().rotation.y += PI
+				get_parent().upheld = true
+				get_parent().set_input_change(false, false, true)
 			elif (Portal2.facing == 'd'):
 				pass
 		elif (Portal1.facing == 'd'):
 			if (Portal2.facing == 'l'):
-				get_parent().rotation.y += PI / 2
+				get_parent().downheld = true
+				get_parent().set_input_change(true, false, false)
 			elif (Portal2.facing == 'r'):
-				get_parent().rotation.y -= PI / 2
+				get_parent().downheld = true
+				get_parent().set_input_change(true, true, true)
 			elif (Portal2.facing == 'u'):
 				pass
 			elif (Portal2.facing == 'd'):
-				get_parent().rotation.y += PI
+				get_parent().downheld = true
+				get_parent().set_input_change(false, false, true)
 		
 	else:
 		if (Portal1.facing == 'l'):
