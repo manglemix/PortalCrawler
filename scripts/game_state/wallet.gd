de8d3d7aec851f978640218f149ad246a332f40b
Najman <najman.husaini@utah.edu>
2024-07-02 17:22:59 -0600

    added wizard hats

--- scripts/game_state/wallet.gd
+++ scripts/game_state/wallet.gd
@@ -3,14 +3,17 @@ extends SaveDataFragment
 
 
 static var coins := 0
+static var wizard_hats := 0
 
 
 func _load_save_data(data: Dictionary) -> void:
 	if "coins" in data:
 		coins = data["coins"]
+	if "wizard_hats" in data:
+		wizard_hats = data["wizard_hats"]
 
 
 func _create_save_data(data: Dictionary) -> void:
 	data["coins"] = coins
-
+	data["wizard_hats"] = wizard_hats
 
